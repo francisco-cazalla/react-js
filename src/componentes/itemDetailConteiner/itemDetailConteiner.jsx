31461d13889d13c6d57a3fddde1f9710dfbcebe0
francisco cazalla <ouyfkjghv@gmail.com>
2024-09-29 19:06:57 -0300

    segunda prentrega2

--- src/componentes/itemDetailConteiner/itemDetailConteiner.jsx
+++ src/componentes/itemDetailConteiner/itemDetailConteiner.jsx
@@ -20,7 +20,7 @@ import {
 
 } from "@chakra-ui/react";
 import { MdLocalShipping } from "react-icons/md";
-import { ItemCount } from "../ItemCount";
+import { ItemCount } from "../itemCount";
 import { useState } from "react";
 import { NavBar } from "../NavBar";
 import { CartWidget } from "../CartWidget";
